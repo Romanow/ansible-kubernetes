9663786b15a423bfd73f05a58d62840a3080158d
Romanov Alex <alexey.romanov@vrbank.ru>
2021-03-16 16:03:49 +0300

    add DO terraform

--- terraform/00-variables.tf
+++ terraform/00-variables.tf
@@ -3,16 +3,23 @@ variable "do_token" {
 }
 
 variable "vm" {
-  type = object({
+  type    = object({
     count  = number
     region = string
+    name   = string
     size   = string
     image  = string
   })
   default = {
-    count = 3
+    count  = 3
     region = "ams3"
-    size = "s-2vcpu-2gb"
-    image = "base-dev-image.13-03-2021"
+    name   = "k8s"
+    size   = "s-2vcpu-2gb"
+    image  = "base-dev-image.13-03-2021"
   }
+}
+
+variable "domain" {
+  type = string
+  default = "romanow-alex.ru"
 }
--- terraform/20-machines.tf
+++ terraform/20-machines.tf
@@ -7,7 +7,7 @@ data "digitalocean_droplet_snapshot" "vm_snapshot" {
 resource "digitalocean_droplet" "vm" {
   count    = var.vm.count
   image    = data.digitalocean_droplet_snapshot.vm_snapshot.id
-  name     = "vm-${count.index}"
+  name     = "vm-${var.vm.name}-${count.index}"
   region   = var.vm.region
   size     = var.vm.size
   tags     = [
@@ -21,7 +21,11 @@ output "instance_ip_addr" {
   description = "The IP addresses of the deployed instances, paired with their IDs."
 }
 
-output "droplet_price" {
-  value       = digitalocean_droplet.vm.*.price_hourly
-  description = "Droplet price per hour"
+resource "digitalocean_record" "vm-a-record" {
+  count  = var.vm.count
+  name   = count.index == 0 ? "master" : "worker-${count.index}"
+  domain = var.domain
+  type   = "A"
+  ttl    = 300
+  value  = element(digitalocean_droplet.vm.*.ipv4_address, count.index)
 }
